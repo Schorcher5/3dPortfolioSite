c9109804068f910928a2ff30bb5d0072232cd39f
JoaquinCisneros <2014joaquincisneros@gmail.com>
2022-07-05 13:40:51 -0700

    Addded function to intialize the line draws

--- javascript/main.js
+++ javascript/main.js
@@ -10,15 +10,18 @@ import { OrbitControls } from './jsm/controls/OrbitControls.js';
 document.addEventListener("DOMContentLoaded", function () {
   // Set up for the basic scene, camera and renderer
   const scene = new THREE.Scene();
+  const backdrop = document.querySelector('#three-js-background');
 
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 4000);
   const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#three-js-background'),
+    canvas: backdrop,
+    antialias: true
   });
   
   renderer.setPixelRatio( window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.position.setZ(30);
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  camera.position.setZ(1750);
   
   //Set up for the electric lights
 
@@ -58,6 +61,86 @@ document.addEventListener("DOMContentLoaded", function () {
       particleCount = parseInt(value);
       particles.setDrawRange(0, particleCount);
     });
+  }
+
+  function initLines() {
+
+    initGUI();
+
+    container = backdrop
+
+    const controls = new OrbitControls( camera, container);
+    controls.minDistance = 1000;
+    controls.maxDistance = 3000;
+
+    group = new THREE.Group();
+    scene.add(group);
+
+    const helper = new THREE.BoxHelper(new THREE.Mesh(new THREE.BoxGeometry(r,r,r)));
+    helper.material.color.setHex(0x101010);
+    helper.material.blending = THREE.AdditiveBlending;
+    helper.material.transparent = true;
+    group.add(helper);
+
+    const segments = maxParticleCount * maxParticleCount;
+
+    positions = new Float32Array(segments*3);
+    colors = new Float32Array(segments * 3);
+
+    const particleMaterial = new THREE.PointsMaterial({
+      color: 0xFFFFFF,
+      size: 3,
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+      sizeAttenuation: false
+    });
+
+    particles = new THREE.BufferGeometry();
+    particlePositions = new Float32Array(maxParticleCount * 3);
+
+    for( let i = 0; i< maxParticleCount; i++){
+      const x = Math.random() * r-r/2;
+      const y = Math.random() * r-r/2;
+      const z = Math.random() * r-r/2;
+
+      particlePositions[i*3] = x;
+      particlePositions[i*3+1] = y;
+      particlePositions[i*3+2] = z;
+
+      particlesData.push({
+        velocity: new THREE.Vector3(-1 + Math.random()*2, -1 + Math.random()*2, -1 + Math.random()*2),
+        numConnections:0
+      });
+    }
+
+    particles.setDrawRange( 0, particleCount);
+    particles.setAttribute( 'position', new THREE.BufferAttribute( particlePositions, 3).setUsage(THREE.DynamicDrawUsage));
+
+    pointCloud = new THREE.Points(particles, particlePositions);
+    group.add(pointCloud);
+    const geometry = new THREE.BufferGeometry();
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3).setUsage(THREE.DynamicDrawUsage));
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors,3).setUsage(THREE.DynamicDrawUsage));
+    geometry.computeBoundingSphere();
+    geometry.setDrawRange(0,0);
+
+    const material = new THREE.LineBasicMaterial({
+      vertexColors:true,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    });
+
+    linesMesh = new THREE.LineSegments(geometry, material);
+    group.add(lineMesh);
+
+    container.appendChild(renderer.domElement);
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+    
+
+
   }
 
   //Dummy torus for testing
@@ -104,6 +187,7 @@ document.addEventListener("DOMContentLoaded", function () {
     renderer.render(scene ,camera);
   }
   
+  initLines();
   animate();
   
   
