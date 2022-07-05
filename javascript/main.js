d5c3de8eec66d4c301abe5d5c894d09e4843cfbf
JoaquinCisneros <2014joaquincisneros@gmail.com>
2022-07-05 12:55:53 -0700

    Added vars and init gui for line drawing

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,12 @@ import '../css/style.css'
 import * as THREE from 'three';
 import { MathUtils } from 'three';
 
+import Stats from '.jsm/libs/stats.module.js';
+import { GUI } from './jsm/libs/lil-gui.module.min.js';
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
+
 document.addEventListener("DOMContentLoaded", function () {
+  // Set up for the basic scene, camera and renderer
   const scene = new THREE.Scene();
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -15,12 +20,55 @@ document.addEventListener("DOMContentLoaded", function () {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.position.setZ(30);
   
+  //Set up for the electric lights
+
+  let group, container, stats , positions, colors, 
+  particles, pointCLoud, particlePositions, linesMesh;
+  const particlesData = [];
+  
+  const maxParticleCount = 1000;
+  let particleCount = 500;
+  const r =  800;
+  const rHalf = r/2;
+
+  const effectController = {
+    showDots: true,
+    showLines: true,
+    minDistance: 150,
+    limitConnections: false,
+    maxConnection: 20,
+    particleCount: 500
+  }
+
+  function initGUI(){
+    const gui = new GUI();
+    
+    gui.add(effectController, 'showDots').onChange((value) =>{
+      pointCLoud.visible = value;
+    });
+
+    gui.add(effectController, 'showLines').onChange((value) => {
+      linesMesh.visible = value;
+    });
+
+    gui.add(effectController, 'minDistance', 10, 300);
+    gui.add(effectController, 'limitConnections');
+    gui.add(effectController, 'maxConnections', 0, 30, 1);
+    gui.add(effectController, 'particleCount', 0, maxParticleCount, 1).onChange((value) => {
+      particleCount = parseInt(value);
+      particles.setDrawRange(0, particleCount);
+    });
+  }
+
+  //Dummy torus for testing
   const torus = new THREE.Mesh( 
     new THREE.TorusGeometry(10, 3, 16, 100), 
     new THREE.MeshStandardMaterial({color:0xFF6347}));
   
   scene.add(torus);
   
+
+  //Basic lighting
   const pointLight = new THREE.PointLight(0xffffff);
   pointLight.position.set(20,20,20);
   
@@ -28,6 +76,8 @@ document.addEventListener("DOMContentLoaded", function () {
   
   scene.add(pointLight, ambientLight);
   
+
+  //Adding Stars to the scene in random places
   function addStars() {
     const star = new THREE.Mesh(
       new THREE.SphereGeometry(0.25,24,24),
@@ -42,6 +92,8 @@ document.addEventListener("DOMContentLoaded", function () {
   
   Array(200).fill().forEach(addStars);
   
+
+  //Animation loop where the rendering take place
   function animate(){
     requestAnimationFrame(animate);
   
